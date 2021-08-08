86149cf3c14a9eaf765da0cc11675f74021a83fa
lutheryap <59101245+lutheryap@users.noreply.github.com>
2021-08-08 15:28:32 -0400

    Add files via upload

---
+++ ebci_test.R
@@ -0,0 +1,45 @@
+## Testing if Stata output checks out with R
+
+require("testthat")
+require("ebci")
+
+# Load the output from Stata
+eb_out1 <- haven::read_dta(file = "eb_out1.dta")
+eb_out1 <- eb_out1[is.na(eb_out1$EB_df1)==FALSE ,]
+eb_out2 <- haven::read_dta(file = "eb_out2.dta")
+eb_out2 <- eb_out2[is.na(eb_out2$EB_df1)==FALSE ,]
+eb_out3 <- haven::read_dta(file = "eb_out3.dta")
+eb_out3 <- eb_out3[is.na(eb_out3$EB_df1)==FALSE ,]
+
+############
+# Load data and test if output are equal
+load("cz.rda")
+
+test_that("Compare ebreg1", {
+   eb_out1 <- as.matrix(unname(eb_out1))
+   Rout <- ebci(theta25 ~ stayer25, cz[cz$state=="NY", ],
+                se25, 1/se25^2, alpha=0.05, fs_correction = "none", wopt=TRUE)
+   REBdf <- as.matrix(unname(Rout$df))
+   diff <- eb_out1 - REBdf
+   expect_equal(max(abs(diff)), 0, tolerance=1e-4)
+})
+
+test_that("Compare ebreg2", {
+   eb_out2 <- as.matrix(unname(eb_out2))
+   Rout <- ebci(theta25 ~ stayer25, cz[cz$state=="NY", ],
+                se25, 1/se25^2, alpha=0.1, wopt=TRUE)
+   REBdf <- as.matrix(unname(Rout$df))
+   diff <- eb_out2 - REBdf
+   expect_equal(max(abs(diff)), 0, tolerance=1e-4)
+})
+
+test_that("Compare ebreg3", {
+   eb_out3 <- as.matrix(unname(eb_out3))
+   Rout <- ebci(theta25 ~ stayer25, cz[cz$state=="FL", ],
+                se25, 1/se25^2, alpha=0.1, kappa = 10, wopt=TRUE)
+   REBdf <- as.matrix(unname(Rout$df))
+   diff <- eb_out3 - REBdf
+   expect_equal(max(abs(diff)), 0, tolerance=1e-4)
+})
+
+
---
+++ ebreg_test.do
@@ -0,0 +1,31 @@
+* Do file to test out function
+* Save EB_df to folder to be called by R
+
+clear all
+qui do ebci_v3.ado
+use cz, replace
+gen wgt = 1/se25^2
+save cz1, replace
+
+ebreg theta25 stayer25 if state == "NY", se(se25) wopt weights(wgt) fs_correction(none) 
+matrix EB_df = e(EB_df)
+svmat EB_df
+keep EB_df*
+save eb_out1, replace
+
+use cz1, replace
+ebreg theta25 stayer25 if state == "NY", se(se25) wopt weights(wgt) alpha(0.1)
+matrix list e(w_opt)
+matrix EB_df = e(EB_df)
+svmat EB_df
+keep EB_df*
+save eb_out2, replace
+
+use cz1, replace
+ebreg theta25 stayer25 if state == "FL", se(se25) wopt weights(wgt) kappa(10) alpha(0.1)
+matrix EB_df = e(EB_df)
+svmat EB_df
+keep EB_df*
+save eb_out3, replace
+
+
